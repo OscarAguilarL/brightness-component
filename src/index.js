330aeb66beee8d0274ab261dfcb41d3814056f23
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-07-22 15:59:38 -0500

    Add functionality to component

---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { init } from './js/BrightnessInput';
+import './styles.css';
+
+init('app');
---
+++ src/js/BrightnessInput.js
@@ -0,0 +1,40 @@
+import '../css/BrightnessInput.css';
+
+const wrapper = document.querySelector('.wrapper');
+const body = document.body;
+let brightInput;
+
+const BrightnessInput = () => {
+  const html = `
+    <form>
+      <input
+        type="range"
+        name="brightness"
+        id="brightness"
+        value="0"
+        min="1"
+        max="100"
+        step="1"
+      />
+    </form>
+  `;
+
+  const div = document.createElement('div');
+  div.innerHTML = html;
+  wrapper.appendChild(div);
+
+  brightInput = document.querySelector('#brightness');
+};
+
+const events = () => {
+  brightInput.addEventListener('input', (event) => {
+    body.style.backgroundColor = `hsl(248, 22%, ${brightInput.value}%)`;
+  });
+};
+
+const init = () => {
+  BrightnessInput();
+  events();
+};
+
+export { init };
