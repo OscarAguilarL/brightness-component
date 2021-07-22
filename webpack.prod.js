95ec27a9a09ace47384b5429158c213591c96cd5
Oscar Aguilar <aguilop.oscar@gmail.com>
2021-07-22 17:17:41 -0500

    Production build

--- webpack.prod.js
+++ webpack.prod.js
@@ -47,6 +47,10 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+        use: ['url-loader?limit=100000'],
+      },
     ],
   },
 
